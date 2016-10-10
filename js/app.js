8c06850bcf1b53a03a41a5a5785ca784f2de6efd
stevenliao <Stvnliao@yahoo.com>
2016-10-09 20:11:53 -0400

    Clicks and ifs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
 
 var app = new Vue
 ({
-    el: "#app",
-    data: {
-        message: "Hello"
+    el: '#app',
+    
+    data: 
+    {
+        message: 'Hello',
+        seen: true,
+        url: 'http://stevenliao.tech'
+    },
+    
+    methods: 
+    {
+        toggleSeen: function()
+        {
+            this.seen = !this.seen;
+        }
+    },
+    
+    filters: 
+    {
+        uppercase: function(value)
+        {
+            if (!value) return "";
+            return value.toUpperCase();
+        }
     }
 });
