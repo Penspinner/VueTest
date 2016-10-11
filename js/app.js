aa0a5ef5b7728c0c72e799b2d1f62e112360a765
stevenliao <Stvnliao@yahoo.com>
2016-10-11 00:13:49 -0400

    Experiment with other method components

--- js/app.js
+++ js/app.js
@@ -7,9 +7,34 @@ var app = new Vue
     {
         message: 'Hello',
         seen: true,
-        url: 'http://stevenliao.tech'
+        url: 'http://stevenliao.tech',
+        firstName: 'Steven',
+        lastName: 'Liao'
     },
     
+    // Re-evaluated when dependencies change
+    // Use computed when you want to cache
+    computed:
+    {
+        fullName: 
+        {
+            get: function()
+            {
+                return this.firstName + ' ' + this.lastName;
+            },
+            
+            // Example: app.fullName = 'Rick Astley' invokes this setter
+            set: function(newName)
+            {
+                var nameSplit = newName.split(' ');
+                this.firstName = nameSplit[0];
+                this.lastName = nameSplit[nameSplit.length - 1];
+            }
+        }
+    },
+    
+    // Method invocations always happen on re-renders
+    // Use method when you don't want to cache
     methods: 
     {
         toggleSeen: function()
@@ -18,12 +43,24 @@ var app = new Vue
         }
     },
     
+    // Methods that change data based on other data
+    // It is usually better to use computed 
+    watch:
+    {
+        
+    },
+    
     filters: 
     {
         uppercase: function(value)
         {
             if (!value) return "";
             return value.toUpperCase();
+        },
+        reverse: function(value)
+        {
+            if (!value) return "";
+            return value.split('').reverse().join('');
         }
     }
 });
